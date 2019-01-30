3de7e6f279badefd1c6b56e6bd74e0032b5fc4ef
Patrick Kelly <patrick@pkelly.co>
2019-01-30 20:25:37 +0000

    completed exercise 29

---
+++ Exercises/Section2/e29.java
@@ -0,0 +1,13 @@
+public class EvenNumbers {
+
+  public static void main(String[] args) {
+    int a = 1;
+
+    while(a <= 100) {
+      if (a % 2 == 0) {
+        System.out.println(a);
+      }
+      a++;
+    }
+  }
+}
