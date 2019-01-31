5941a0008b95522aa78a5e530bd85ff273e02062
Patrick Kelly <patrick@pkelly.co>
2019-01-31 20:25:14 +0000

    completed exercise 41

---
+++ Exercises/Section2/e41.java
@@ -0,0 +1,32 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessingNumberGame {
+
+  public static void main(String[] args) {
+    Scanner reader = new Scanner(System.in);
+    int numberDrawn = drawNumber();
+    int guessCount = 0;
+
+    while(true) {
+      System.out.println("Guess a number:");
+      int guess = Integer.parseInt(reader.nextLine());
+
+      if(guess < numberDrawn) {
+        System.out.println("The number is greater, guesses made: " + guessCount);
+        guessCount++;
+      } else if (guess > numberDrawn) {
+        System.out.println("The number is lesser, guesses made: " + guessCount);
+        guessCount++;
+      } else {
+        System.out.println("Congratulations, your guess is correct!");
+        break;
+      }
+    }
+
+  }
+  
+  private static int drawNumber() {
+    return new Random().nextInt(101);
+  }
+}
