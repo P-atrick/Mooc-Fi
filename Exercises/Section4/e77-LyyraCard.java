a6ff0d2be895168d49b332c146d9aadc7f48816a
Patrick Kelly <patrick@pkelly.co>
2019-02-02 15:40:11 +0000

    completed exercises

---
+++ Exercises/Section4/e77-LyyraCard.java
@@ -0,0 +1,32 @@
+public class LyyraCard {
+  private double balance;
+
+  public LyyraCard(double balance) {
+    this.balance = balance;
+  }
+
+  public void loadMoney(double amount) {
+    if ((this.balance + amount) >= 150 && amount >=0) {
+      this.balance = 150.00;
+    } else if (amount >= 0) {
+      this.balance = this.balance + amount;
+    }
+  }
+
+  public void payEconomical() {
+    if (this.balance >= 2.5) {
+      this.balance = this.balance - 2.50;
+
+    }
+  }
+
+  public void payGourmet() {
+    if(this.balance >= 4) {
+      this.balance = this.balance - 4.00;
+    }
+  }
+
+  public String toString() {
+    return "The card has " + this.balance + " euros";
+  }
+}
